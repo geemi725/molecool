34aa73433c846183ea09a9ebcbe8c0eb962bf0fd
geemi725 <gwellawatte@gmail.com>
2020-08-04 14:34:16 -0400

    add restructured files

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -5,7 +5,11 @@ A python package for analysing and visualizing molecular structures for MSF boot
 
 # Add imports here
 from .functions import *
+from .measure  import calculate_distance, calculate_angle
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
 
+import molecool.io
 # Handle versioneer
 from ._version import get_versions
 versions = get_versions()
